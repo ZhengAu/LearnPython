32fd13c70e56b058cedbc752feb49c4fbf68d326
ZPJ <598359997@qq.com>
2018-02-09 09:52:43 +0800

    initial and add some .py

---
+++ first.py
@@ -0,0 +1,27 @@
+# def pnum():
+#     num = 1
+#     print(num)
+
+
+# num = 10
+# pnum()
+# print(num)
+
+a = 9999
+b = 10
+print(a + b)
+print(a - b)
+print(a**b)  # a的b次幂
+print(a // b)  # a除以b，向下取整
+print(a % 6)  # 求余
+print()
+c = 9999
+print(not a)
+
+print(a != c)
+d = 10001
+print(~d)
+print(a is b)
+print(a is c)
+print(a is not c)
+print()
---
+++ python.py
@@ -0,0 +1,13 @@
+a = 123
+b = 123
+c = 123
+print('a =', a)
+print(b)
+print(c)
+print(id(a))
+print(id(c))
+print(id(b))
+
+b = 123456
+print(b)
+print(id(b))
