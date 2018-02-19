90ce87804cda0a13357e407f4ff538b731645107
ZPJ <598359997@qq.com>
2018-02-20 00:22:28 +0800

    class test

---
+++ LearnClass/1-class.py
@@ -0,0 +1,17 @@
+# encoding=UTF-8
+# class的定义
+# Noted:方法中的self不能省略
+
+class Student:
+
+	def running(self):
+		print("I'm running...")
+	
+	def eating(self):
+		print("I'm eating...")
+
+stu1=Student()
+
+stu1.running()
+stu1.eating()
+
