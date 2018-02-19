fd5328f9f83b50391dd5e8b87b4d1ef2064159b3
ZPJ <598359997@qq.com>
2018-02-19 13:58:16 +0800

    递归

---
+++ 16-递归.py
@@ -0,0 +1,18 @@
+# encoding=utf-8
+
+# 递归函数的运用
+# 注意：要有退出函数的标志，即结束标志
+
+def test1(num):
+	if num>1:
+		result = num+test1(num-1)
+	else:
+		result = 1
+	return result
+
+num=int(input("Please input num: "))
+
+result = test1(num)
+
+print(result)
+
